dd49c5b2943299cb3528e70168ca0fb56259dee5
zwx-tech <sdebicki42@uczen.tl.krakow.pl>
2023-10-10 11:44:26 +0200

    lesson 6

--- 04-Grades/app.py
+++ 04-Grades/app.py
@@ -1,15 +1,22 @@
 # imports
+import datetime
+
 from flask import Flask, render_template, session, redirect
 from flask_bs4 import Bootstrap
+from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import json
+import datetime
 
 # config
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'we live we love we lie'
+moment = Moment(app)
+date = datetime.datetime.now()
+
 # load 'db'
 with open("data/users.json") as f:
     users = json.load(f)
@@ -47,13 +54,15 @@ def login():
 def dashboard():
     if not session.get('userLogin'):
         return redirect('login')
-    return render_template('dashboard.html', title="Logowaie", userLogin=session.get('userLogin'))
+    return render_template('dashboard.html', title="Logowaie", userLogin=session.get('userLogin'), date=date)
 
 @app.route('/logOut')
 def logOut():
     if session.get('userLogin'):
         session.pop('userLogin')
     return redirect('login')
+
+
 # run
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ 04-Grades/static/js/clock.js
@@ -0,0 +1,11 @@
+function updateClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  const timeElement = document.getElementById("time");
+  timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+updateClock()
+setInterval(updateClock, 1000);
